ff5c98831468e08ee93ebf45851e0ac31cc403b0
pak215 <pak215.izzle@gmail.com>
2017-04-03 21:36:46 -0500

    Updated query functions

--- app/query_records.py
+++ app/query_records.py
@@ -8,7 +8,32 @@ import json
 from insert_records import init_session
 from models import Restaurants, Locations, Food_Types, Reviews, Base
 
-def query_restaurants_filter(session_obj, **kwargs):
-	restaurants = session_obj.query(Restaurants).filter_by(**kwargs)
-	return jsonify(json_list = qryresult.all())
+def query_restaurant_by_id(session_obj, i):
+	restaurant = session_obj.query(Restaurants).filter_by(id = i).first()
+	return jsonify(json_list = restaurant)
+
+def query_all_restaurants(session_obj):
+	return jsonify(json_list = session_obj.query(Restaurants).all())
+
+def query_location_by_zip(session_obj, z):
+	location = session_obj.query(Locations).filter_by(zip = z).first()
+	return jsonify(json_list = location)
+
+def query_all_locations(session_obj):
+	return jsonify(json_list = session_obj.query(Locations).all())
 	
+def query_food_type_by_name(session_obj, n):
+	food_types = session_obj.query(Food_Types).filter_by(food_type = n).first()
+	return jsonify(json_list = food_types)
+
+def query_all_food_types(session_obj):
+	food_types = session_obj.query(Food_Types).all()
+	return jsonify(json_list = food_types)
+
+def query_review_by_id(session_obj, id):
+	review = session_obj.query(Reviews).filter_by(review_id = id).first()
+	return jsonify(json_list = review)
+
+def query_all_reviews(session_obj):
+	reviews = session_obj.query(Reviews).all()
+	return jsonify(json_list = reviews)
