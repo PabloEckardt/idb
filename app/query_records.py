daff29361eed9844830a55cb8e4807f1a5f7e381
Parker Kruzel <pak215.izzle@gmail.com>
2017-04-03 19:37:23 -0500

    Began making query_records.py

---
+++ app/query_records.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# pylint: disable = bad-whitespace
+# pylint: disable = invalid-name
+# pylint: disable = missing-docstring
+
+from insert_records import init_session
+
+def query_restaurant :
+	
+
