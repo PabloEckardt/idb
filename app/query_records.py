6c9c223eaf9e05ebcb89ca4e1c65df984c003621
pak215 <pak215.izzle@gmail.com>
2017-04-03 20:07:07 -0500

    updated query_records

--- app/query_records.py
+++ app/query_records.py
@@ -4,8 +4,12 @@
 # pylint: disable = invalid-name
 # pylint: disable = missing-docstring
 
+import json
 from insert_records import init_session
+from models import Restaurants, Locations, Food_Types, Reviews, Base
 
-def query_restaurant :
+def query_restaurants(session_obj, **kwargs):
+	restaurant_1 = session_obj.query(Restaurants)
+	return jsonify(json_list = qryresult.all())
 	
 
