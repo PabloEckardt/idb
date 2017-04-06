31a18d9a3cae79b642df1b28ae34d907b1f0f3fc
pabloeckardt <pablo0168@hotmail.com>
2017-04-05 23:41:30 -0500

    reloader off

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = app.create_app(config)
 # This is only used when running locally. When running live, gunicorn runs
 # the application.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
